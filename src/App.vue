d53f544891c9a55ef4caff2cac79025ba9986104
nisino25 <nisino25@gmail.com>
2022-04-18 00:17:06 +0900

    turn off auto zooming

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
       <link rel="apple-touch-icon" sizes="180x180" href="https://i.ibb.co/SJkgPg9/android-chrome-512x512.png">
   <link rel="icon" type="image/png" sizes="32x32" href="https://i.ibb.co/SJkgPg9/android-chrome-512x512.png">
   <link rel="icon" type="image/png" sizes="16x16" href="https://i.ibb.co/SJkgPg9/android-chrome-512x512.png">
